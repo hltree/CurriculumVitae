d90a9bce4f1542e1c62943c62ff856a3f20b098a
hltree <35206336+hltree@users.noreply.github.com>
2020-11-11 00:25:54 +0900

    chore: change repository name

---
+++ CurriculumVitae/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<style lang="scss">
+.width100 {
+  width: 100%;
+}
+</style>
